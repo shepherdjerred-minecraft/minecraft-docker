1b1e28d986a646709d409ac984efe0808ae1948f
Jerred Shepherd <shepherdjerred@gmail.com>
2020-06-28 15:21:11 -0700

    Add more Minecraft versions

--- download-jar.sh
+++ download-jar.sh
@@ -10,6 +10,9 @@ fi
 if [ "$version" = "1.16.1" ]
   then
     url="https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar"
+elif [ "$version" = "1.15.2" ]
+  then
+    url="https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar"
 fi
 
 wget "$url"

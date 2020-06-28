da46887a4e00d71360ca79d0534810603cbdcfc1
Jerred Shepherd <shepherdjerred@gmail.com>
2020-06-28 15:18:15 -0700

    Add support for multiple Minecraft versions

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
 FROM openjdk:jdk-slim
 
+RUN  apt-get update \
+  && apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+
 RUN useradd -ms /bin/bash minecraft
 USER minecraft
 RUN mkdir /home/minecraft/server
 WORKDIR /home/minecraft
 
-RUN download-jar.sh
+ADD download-jar.sh .
+ARG MINECRAFT_VERSION=latest
+RUN ./download-jar.sh $MINECRAFT_VERSION
 
 WORKDIR /home/minecraft/server
 ENTRYPOINT ["java"]
--- download-jar.sh
+++ download-jar.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [ "$MINECRAFT_VERSION" = "latest" ]
+version="$1"
+
+if [ "$version" = "latest" ]
   then
-    MINECRAFT_VERSION = "1.16.1"
+    version="1.16.1"
 fi
 
-if [ "$MINECRAFT_VERSION" = "1.16.1" ]
+if [ "$version" = "1.16.1" ]
   then
-    DOWNLOAD_URL = "https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar"
+    url="https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar"
 fi
 
-wget $DOWNLOAD_URL
+wget "$url"
 chown minecraft:minecraft server.jar
+

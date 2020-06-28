f77e560ec8c7e7d041a868e2cf5996f15f95624c
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-06-28 14:41:36 -0700

    Update download-jar.sh

--- download-jar.sh
+++ download-jar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$MINECRAFT_VERSION" = "latest" ]
+  then
+    MINECRAFT_VERSION = "1.16.1"
+fi
+
 if [ "$MINECRAFT_VERSION" = "1.16.1" ]
   then
     DOWNLOAD_URL = "https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar"

b35f2cd59983040e6eac4b64e66f0e1b6027a73e
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-06-28 14:40:37 -0700

    Update and rename set-download-url.sh to download-jar.sh

---
+++ download-jar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$MINECRAFT_VERSION" = "1.16.1" ]
+  then
+    DOWNLOAD_URL = "https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar"
+fi
+
+wget $DOWNLOAD_URL
+chown minecraft:minecraft server.jar
